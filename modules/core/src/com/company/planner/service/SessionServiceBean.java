ba8fdffccbeb0bd29b1892f536901869905b0b77
Dominic Diamante <dominic.diamante@gmail.com>
2021-10-16 23:10:41 +1100

    Wrote the implementation of rescheduleSession

--- modules/core/src/com/company/planner/service/SessionServiceBean.java
+++ modules/core/src/com/company/planner/service/SessionServiceBean.java
@@ -4,12 +4,15 @@ import com.company.planner.entity.Session;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @Service(SessionService.NAME)
 public class SessionServiceBean implements SessionService {
 
     @Override
     public Session rescheduleSession(Session session, LocalDateTime newStartDate) {
+        LocalDateTime dayStart = newStartDate.truncatedTo(ChronoUnit.DAYS).withHour(8);
+        LocalDateTime dayEnd = newStartDate.truncatedTo(ChronoUnit.DAYS).withHour(19);
         return null;
     }
 }
