bf9d87f638b1610a4afd358a385b441bab374271
Dominic Diamante <dominic.diamante@gmail.com>
2021-10-16 21:29:03 +1100

    Added Session entity
    Added the Session entity through the cuba studio wizard. So every time we add an entity, cuba platform creates a new class for the entity under the global module which is jpa annotated. It then registers that in persistence.xml and creates entry in messages.properties for the string representation fo the attributes and the class itself.

---
+++ modules/global/src/com/company/planner/entity/Session.java
@@ -0,0 +1,82 @@
+package com.company.planner.entity;
+
+import com.haulmont.chile.core.annotations.MetaProperty;
+import com.haulmont.chile.core.annotations.NamePattern;
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Table(name = "PLANNER_SESSION")
+@Entity(name = "planner_Session")
+@NamePattern("%s|description")
+public class Session extends StandardEntity {
+    private static final long serialVersionUID = 3271255497931152196L;
+
+    @NotNull
+    @Column(name = "TOPIC", nullable = false)
+    private String topic;
+
+    @NotNull
+    @Column(name = "START_DATE", nullable = false)
+    private LocalDateTime startDate;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "SPEAKER_ID")
+    private Speaker speaker;
+
+    @NotNull
+    @Column(name = "DURATION", nullable = false)
+    private Integer duration;
+
+    @Lob
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public Speaker getSpeaker() {
+        return speaker;
+    }
+
+    public void setSpeaker(Speaker speaker) {
+        this.speaker = speaker;
+    }
+
+    @MetaProperty
+    public LocalDateTime getEndDate() {
+        return null;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+}
