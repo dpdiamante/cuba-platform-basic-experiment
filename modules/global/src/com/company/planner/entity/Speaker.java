a1964903350ff418ccd2249275acb4af53d40b75
Dominic Diamante <dominic.diamante@gmail.com>
2021-10-16 19:41:16 +1100

    Added attributes to Speaker entity
    Added the first name, last name and email attributes to the Speaker entity through the cuba studio wizard. As you can see, cuba platform generated the text representation for each attribute in messages.properties and also mutated the Speaker java class with the jpa annotated attributes. I would probably have to learn JPA at this point as an added learning.

--- modules/global/src/com/company/planner/entity/Speaker.java
+++ modules/global/src/com/company/planner/entity/Speaker.java
@@ -1,12 +1,53 @@
 package com.company.planner.entity;
 
+import com.haulmont.chile.core.annotations.NamePattern;
 import com.haulmont.cuba.core.entity.StandardEntity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 @Table(name = "PLANNER_SPEAKER")
 @Entity(name = "planner_Speaker")
+@NamePattern("%s %s|firstName,lastName")
 public class Speaker extends StandardEntity {
     private static final long serialVersionUID = 6405448562278922979L;
+
+    @NotNull
+    @Column(name = "FIRST_NAME", nullable = false)
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @NotNull
+    @Column(name = "EMAIL", nullable = false, unique = true, length = 1024)
+    @Email(message = "{msg://planner_Speaker.email.validation.Email}")
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
