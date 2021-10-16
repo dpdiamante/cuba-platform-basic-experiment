387ce6aec13d44bee3741665775104d76afb1638
Dominic Diamante <dominic.diamante@gmail.com>
2021-10-16 22:10:03 +1100

    Generated the database scripts
    Generated the database scripts for our sample application. Again the database scripts are created with the help of cuba studio.

---
+++ modules/core/db/update/postgres/21/10/211016-1-createSpeaker.sql
@@ -0,0 +1,16 @@
+create table PLANNER_SPEAKER (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    FIRST_NAME varchar(255) not null,
+    LAST_NAME varchar(255),
+    EMAIL varchar(1024) not null,
+    --
+    primary key (ID)
+);
